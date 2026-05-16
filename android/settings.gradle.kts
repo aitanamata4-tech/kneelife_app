a8de713a7b96a878fb2fbd98a4e82703e70faeb1
laiagomezjaner <laiagomezjaner@gmail.com>
2026-05-16 11:40:01 +0200

    App ja funciona al mobil

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,4 +1,14 @@
 pluginManagement {
+    val flutterSdkPath = {
+        val properties = java.util.Properties()
+        file("local.properties").inputStream().use { properties.load(it) }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+        assert(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+        flutterSdkPath
+    }()
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
     repositories {
         google()
         mavenCentral()
@@ -7,18 +17,10 @@ pluginManagement {
 }
 
 plugins {
-    id "dev.flutter.flutter-gradle-plugin" version "1.0.0" apply false
-    id "com.android.application" version "8.1.0" apply false
-    id "org.jetbrains.kotlin.android" version "1.8.22" apply false
-    id "com.google.gms.google-services" version "4.3.15" apply false
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    // id("com.google.gms.google-services") version "4.4.1" apply false
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-include ":app"
+include(":app")
